3c9777b6fc3a4aae5cb2fa1fff2a4d8fcceac418
SayYay <frontier.the.frontier@gmail.com>
2021-09-24 00:41:53 +0900

    Flask Training!!

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect # 追加で2つimportする
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+
+
+# @app.route('/')  # GETメソッドしか受け取れない
+@app.route('/', methods=["GET", "POST"])  # GETとPOSTを受け取れる
 def index():
     return render_template("index.html")
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 
 if __name__ == "__main__":
